df21a9bbd8bb59731b5eba9d18627c506d207327
JOELSON FREITAS DE OLIVEIRA <joelsonfoliveira@gmail.com>
2022-11-26 21:42:34 -0300

    update

---
+++ Moedas/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Moedas
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+
+      decimal valor = 10536.25m;
+      System.Console.WriteLine(valor);
+    }
+  }
+}
