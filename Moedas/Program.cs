fe7ac4210d88114316b53d11ac40b840e5d86a24
JOELSON FREITAS DE OLIVEIRA <joelsonfoliveira@gmail.com>
2022-11-26 22:02:46 -0300

    adicionando novos tratamentos para moedas

--- Moedas/Program.cs
+++ Moedas/Program.cs
@@ -8,9 +8,31 @@ namespace Moedas
     {
       Console.Clear();
 
-      decimal valor = 10536.25m;
+      decimal valor = 10536.75m;
 
-      System.Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+      System.Console.WriteLine(
+        valor.ToString(
+          "C", CultureInfo.CreateSpecificCulture("pt-BR")
+        )
+      ); // imprime em real do Brasil
+
+      System.Console.WriteLine(
+        String.Format(
+          "{0:C}", valor
+        )
+      ); // imprime em real do Brasil
+
+      System.Console.WriteLine(
+        Math.Round(valor)
+      ); // arredonda o valor para cima ou para baixo dependendo da aproximação
+
+      System.Console.WriteLine(
+        Math.Ceiling(valor)
+      ); // arredonda o valor para cima
+
+      System.Console.WriteLine(
+        Math.Floor(valor)
+      ); // arredonda o valor para baixo
     }
   }
 }
